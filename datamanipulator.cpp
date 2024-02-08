7c5c35831a3167fba4b69e155b44cd871f51b933
divyasanjanavarma <158834784+divyasanjanavarma@users.noreply.github.com>
2024-02-08 11:16:18 +0530

    Add files via upload

---
+++ datamanipulator.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include <iomanip>
+using namespace std;
+int main()
+{
+
+float num = 2.5;
+cout<<"Hello"<<endl;
+cout<<"c++"<<ends;
+cout<<setw(10)<<"Hi"<<endl;
+cout<<setw(20)<<setfill('.')<<"sanjana"<<endl;
+cout<<setprecision(15)<<flush<<endl;
+} 
