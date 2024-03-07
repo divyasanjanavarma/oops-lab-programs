0df0a62a84d8ec6feae4a7360b4eb6f46b30fc58
divyasanjanavarma <158834784+divyasanjanavarma@users.noreply.github.com>
2024-03-07 10:33:53 +0530

    Create constructor.cpp

---
+++ week-3/constructor.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Student
+{
+public:
+Student()
+{
+string collegeName = "MVGR";
+int collegeCode=33;
+cout<<"CollegeName : "<<collegeName<<endl;
+cout<<"CollegeCode: "<<collegeCode<<endl;
+}
+Student(string fullName, double semPercentage)
+{
+cout<<"FullName : "<<fullName<<endl;
+cout<<"SemPercentage : "<<semPercentage<<endl;
+}
+Student(){}
+};
+int main()
+{
+Student venkat;
+string name;
+float val;
+cout<<"Enter your name : ";
+getline(cin,name);
+cout<<"Enter your sem percentage : ";
+cin>>val;
+Student venkat01(name,val);
+return 0;
+}
