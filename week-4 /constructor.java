3bcbba0eb4f37ff0e58a77dd30e5f2efe9204642
divyasanjanavarma <158834784+divyasanjanavarma@users.noreply.github.com>
2024-03-07 10:54:15 +0530

    Create constructor.java

---
+++ week-4 /constructor.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Student
+{
+public
+Student()
+{
+String collegeName="MVGR";
+int collegeCode=33;
+System.out.print("CollegeName : "+collegeName+"\n"+"CollegeCode :
+"+collegeCode+"\n");
+}
+Student(String fullName,double semPercentage)
+{
+System.out.print("FullName : "+fullName+"\n"+"SemPercentage : "+semPercentage);
+}
+}
+public class Constructor
+{
+public static void main(String[] args)
+{
+@SuppressWarnings("unused")
+Student venkat=new Student();
+Scanner input=new Scanner(System.in);
+System.out.print("FullName : ");
+String fullName=input.nextLine();
+System.out.print("SemPercentage : ");
+double semPercentage=input.nextDouble();
+input.close();
+@SuppressWarnings("unused")
+Student venkat01=new Student(fullName,semPercentage);
+}
+}
