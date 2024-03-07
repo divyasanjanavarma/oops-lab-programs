4ceebacf2dfe2ab5232e0263a673d33f79b59d6c
divyasanjanavarma <158834784+divyasanjanavarma@users.noreply.github.com>
2024-03-07 11:50:51 +0530

    Update classbox.cpp

--- week-3/classbox.cpp
+++ week-3/classbox.cpp
@@ -1,37 +1,55 @@
-#include&lt;iostream&gt;
+#include<iostream>
 using namespace std;
+
 inline void displayWelcomeMessage()
 {
-cout&lt;&lt;&quot;Welcome&quot;&lt;&lt;endl;
+    cout<<"Hello, user!!"<<endl;
 }
+
 class Box
 {
-public:
-float l,b,h;
-void areaofbox(float l,float b,float h)
-{
-float area=2*(l*b+b*h+h*l);
-cout&lt;&lt;&quot;Area of box is : &quot;&lt;&lt;area&lt;&lt;endl;
-}
-friend void displayBoxDimensions(Box);
-void volumeofbox(float l,float b,float h);
+    private:
+        float l;
+        float w;
+        float h;
+    public:
+        void boxArea(float l,float w,float h)
+        {
+            float area=2*((l*w)+(w*h)+(h*l));
+            cout<<"The area of the box : "<<area<<endl;
+        }
+        void boxVolume(float l,float b,float h);
+        friend void displayBoxDimensions(Box d);
+        void set(float length,float width,float height)
+        {
+            l=length;
+            w=width;
+            h=height;
+        }
 };
-void Box :: volumeofbox(float l,float b,float h)
+
+void displayBoxDimensions(Box d)
 {
-float volume=l*b*h;
-cout&lt;&lt;&quot;Volume of box is : &quot;&lt;&lt;volume&lt;&lt;endl;
+    cout<<"The dimensions of the box : "<<d.l<<" X "<<d.w<<" X "<<d.h<<endl;
 }
-void displayBoxDimensions(Box obj)
+
+//member function outside the class
+void Box::boxVolume(float l,float b,float h)
 {
-cout&lt;&lt;&quot;length =&quot;&lt;&lt;obj.l&lt;&lt;endl;
-cout&lt;&lt;&quot;breadth =&quot;&lt;&lt;obj.b&lt;&lt;endl;
-cout&lt;&lt;&quot;Width=&quot;&lt;&lt;obj.h&lt;&lt;endl;
+    float volume=l*b*h;
+    cout<<"The volume of the box : "<<volume<<endl;
 }
+
 int main()
 {
-Box cuboid;
-cout&lt;&lt;&quot;enter length,breadth and width of the cuboid : &quot;&lt;&lt;endl;
-cin&gt;&gt;cuboid.l&gt;&gt;cuboid.b&gt;&gt;cuboid.h;
-displayWelcomeMessage();
-displayBoxDimensions(cuboid);
-cuboid.volumeofbox(cuboid.l,cuboid.b,cuboid.h);
+    Box b;
+    float length,width,height;
+    cout<<"Enter the length, width, and height : ";
+    cin>>length>>width>>height;
+    b.set(length,width,height);
+    b.boxArea(length,width,height);
+    b.boxVolume(length,width,height);
+    displayBoxDimensions(b);
+    displayWelcomeMessage();
+    return 0;
+}
